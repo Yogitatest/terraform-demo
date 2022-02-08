f315199a7523d5f6f35e756fb5b4ca70bd6cedd3
Yogitatest <97597212+Yogitatest@users.noreply.github.com>
2022-02-08 20:19:46 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = "us-east-1"
+	type    = string
+    description = "AWS Region to deploy to"
+}
+
+variable "environment" {
+    type    = string
+    description = "AWS environment to deploy app like dev, stage, prod"
+}
+
+variable "app" {
+    type    = string
+	default = "demo"
+}
+
