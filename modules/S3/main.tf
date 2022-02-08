9889e307ee3fc1d66dc472bc051f82e2a560798b
Yogitatest <97597212+Yogitatest@users.noreply.github.com>
2022-02-09 00:48:32 +0530

    Update main.tf

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -1,8 +1,8 @@
 
 resource "aws_s3_bucket" "bucket" {	
-	bucket  = ${var.bucket}
-	acl     = ${var.acl}
-	tags    = ${var.tags}
+	bucket  = var.bucket
+	acl     = var.acl
+	tags    = var.tags
 	
 	dynamic "versioning" {
       for_each = length(keys(var.versioning)) == 0 ? [] : [var.versioning]
