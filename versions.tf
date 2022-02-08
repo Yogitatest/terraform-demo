85c7a896fffe628131bffa0b480872c7bdee027f
Yogitatest <97597212+Yogitatest@users.noreply.github.com>
2022-02-08 20:57:56 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
   backend "s3" {
     bucket               = "terraform-remote-states"
     key                  = "tf-workspaces/terraform.tfstate"
-    region               = "ap-south-1"
+    region               = "eu-west-1"
   }
 }
